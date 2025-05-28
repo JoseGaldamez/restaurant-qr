7006fd31f2202abc1763a0b3a1b5b5d646896802
José Galdámez <josegaldamez1991@gmail.com>
2025-05-28 17:48:19 -0600

    fix promise params

--- app/menu/[id]/page.tsx
+++ app/menu/[id]/page.tsx
@@ -1,9 +1,9 @@
 import { MenuPublic } from "@/components/MenuPublic/MenuPublic";
 
 interface MenuPageProps {
-    params: {
+    params: Promise<{
         id: string;
-    };
+    }>;
 }
 
 export default async function MenuPage({ params }: MenuPageProps) {
