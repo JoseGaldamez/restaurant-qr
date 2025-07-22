1bea92e5b5ff139659a90022937f3ade6c5faac0
José Galdámez <josegaldamez1991@gmail.com>
2025-07-22 15:03:54 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,8 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         <link
           crossOrigin="anonymous"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
